70ce03b676ec37d7cb3f37deb77e9e6ddecba58a
Ekinvt <71521059+Ekinvt@users.noreply.github.com>
2021-08-13 23:16:21 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,20 +1,27 @@
-
 var firebaseConfig = {
-      apiKey: "AIzaSyCfYD7hRhyprYEdBlQfGSGS9ESGGBaoruA",
-      authDomain: "kwitter-1744a.firebaseapp.com",
-      databaseURL: "https://kwitter-1744a-default-rtdb.firebaseio.com",
-      projectId: "kwitter-1744a",
-      storageBucket: "kwitter-1744a.appspot.com",
-      messagingSenderId: "928825144655",
-      appId: "1:928825144655:web:9b4c1306801d730ef5c13a"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+      apiKey: "AIzaSyDtkcdOcQ3Xv5ZoyhHtgMTxpJ_WJJfuEOg",
+      authDomain: "coooooooooooooolchat.firebaseapp.com",
+      databaseURL: "https://coooooooooooooolchat-default-rtdb.firebaseio.com",
+      projectId: "coooooooooooooolchat",
+      storageBucket: "coooooooooooooolchat.appspot.com",
+      messagingSenderId: "476261673565",
+      appId: "1:476261673565:web:13b075d09819ad88753bb4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });});}
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  
+            });
+      });
+}
 getData();
+
+function logout() {
+      window.location = "index.html";
+}
